950f1240529f83164d71820ffc6b5fd3f14a3283
technoguy3105 <65338147+technoguy3105@users.noreply.github.com>
2020-06-14 11:17:57 +0200

    Add files via upload

--- lib/aitpath.py
+++ lib/aitpath.py
@@ -8,9 +8,8 @@
 #
 # This file is meant to be located inside the ipython extension folder, 
 #
-#     macos       : 'rainer/anaconda3/lib/python 3.7/site-packages/IPython/extensions'
+#     macos       : 'users/USERNAME/anaconda3/lib/python 3.7/site-packages/IPython/extensions'
 #     win32       : C:\ProgramData\Anaconda3\lib\site-packages\IPython\extensions
-#     win32 azure : \Users\DEBUDDSAOperator\Anaconda3\lib\site-packages\IPython\extensions
 #
 # and sets the path ensuring the application libs are found by ipython for execution.
 #
@@ -23,16 +22,13 @@ def operatingSystem():
     return 'darwin' if sys.platform=='darwin' else ('azure' if azure else 'windows' )
 
 def sysUser():
-    platform = operatingSystem()#sys.platform # windows=win32  macos=darwin
+    platform = operatingSystem()
     if platform=='darwin':
-        sys_user = 'rainer'
+        sys_user = 'USERNAME'
     else: # 'win32'
-        sys_user = 'debuddsaoperator' if platform=='azure' else 'rwallwitz'
-        sys_user += '/desktop/root'  
+        sys_user = 'USERNAME'
     return sys_user
 sys_user = sysUser()
 
-sys.path.append('/users/'+sys_user+'/Py/PyLibs/SpiritLibs')
-sys.path.append('/users/'+sys_user+'/Py/PyLibs/SunFlowLibs')
-sys.path.append('/users/'+sys_user+'/Py/PyLibs/TestLibs')
+sys.path.append('/users/'+sys_user+'/Py/SunFlowLibs')
 
